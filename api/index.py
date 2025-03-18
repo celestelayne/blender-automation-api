49ea39e2681fc38b9bab55177f16dede86f86cce
Celeste Layne <layne.celeste@gmail.com>
2025-03-18 13:35:47 -0400

    create route that handles file logic

--- api/index.py
+++ api/index.py
@@ -1,11 +1,22 @@
-from flask import Flask
+# import flask
+from flask import Flask, request, jsonify
 
+# create an app instance
 app = Flask(__name__)
+
+# Get Blender path from Homebrew
+BLENDER_EXEC = "/Applications/Blender.app/Contents/MacOS/Blender"
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+# create the route that handles the file logic
+@app.route('/process_model', methods=['POST'])
+def process_model():
+    file_path = request.json.get("file_path")
+
+    print(file_path)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
